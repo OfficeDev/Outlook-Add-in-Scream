28c87814fd121bddf13575985219c8a0b22fbd87
Andrew Byrne <andrew.j.byrne@hotmail.com>
2015-11-28 02:29:13 -0800

    Added jQuery intellisense, included $ notation.

--- appread/home/home.js
+++ appread/home/home.js
@@ -13,35 +13,33 @@
 })();
 
 function fadeRant(){
-      // Make sure the message shown after a rant it cleared
-      clearMessages();
+  // Make sure the message shown after a rant it cleared
+  clearMessages();
+  
+  // Only post if rant message is not empty
+  if ($('#rantInput').val() == ''){
+    app.showNotification("Nothing to scream","Enter your feelings in the text field before hitting the scream button.");
+  }
+  else {
+  // Store the font size I started with, since I am going to revert to it
+  var originalSize = jQuery('#rantInput').css('fontSize');
+  
+  $('#rantInput').animate({ 
+    opacity: 0.00,
+    fontSize: "1px"
+    }, 1500, function(){
+      // Animation complete - show message
+      $('#rantMessage').animate({opacity: 1.00},500);
       
-      // Only post if rant message is not empty
-      if (jQuery('#rantInput').val() == ''){
-        app.showNotification("Nothing to scream","Enter your feelings in the text field before hitting the scream button.");
-      }
-      else {
-      // Store the font size I started with, since I am going to revert to it
-      var originalSize = jQuery('#rantInput').css('fontSize');
-      
-      jQuery('#rantInput').animate({ 
-        opacity: 0.00,
-        fontSize: "1px"
-        }, 1500, function(){
-          // Animation complete - show message
-          jQuery('#rantMessage').animate({opacity: 1.00},500);
-          
-          // Show input field again
-          jQuery('#rantInput').val('');
-          jQuery('#rantInput').animate({opacity: 1.00, fontSize: originalSize },500);
-        });
-      }
-    };
+      // Show input field again
+      $('#rantInput').val('');
+      $('#rantInput').animate({opacity: 1.00, fontSize: originalSize },500);
+    });
+  }
+};
     
  function clearMessages() {
-   
-   jQuery("#rantMessage").css({ opacity: 0.0 });
-   jQuery('#notification-message').hide();
-   
+   $("#rantMessage").css({ opacity: 0.0 });
+   $('#notification-message').hide();
  }
    
