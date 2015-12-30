579f182718442b2b164691b9c789357c10aa1ed0
anbyrne <anbyrne@microsoft.com>
2015-12-30 15:43:56 -0800

    Check for empty message.

--- appread/home/home.js
+++ appread/home/home.js
@@ -24,74 +24,70 @@
   };
 })();
 
-// Fades whatever the user wrote into "the void".
+// Fades whatever the user wrote into 'the void'.
 function fadeRant(){
   
   // Make sure the message shown after a rant it cleared.
   clearMessages();
   
   // Only post if rant message is not empty.
-  if ($('#rantInput').val() === ''){
-    app.showNotification("Nothing to scream","Enter your feelings in the text field before hitting the scream button.");
+  if ($('#rantInput').val().trim() === ''){
+    app.showNotification('Nothing to scream','Enter your feelings in the text field before hitting the scream button.');
   }
   else {
+    // Store the font size I started with, since I am going to revert to it.
+    var originalSize = jQuery('#rantInput').css('fontSize');
     
-  // Store the font size I started with, since I am going to revert to it.
-  var originalSize = jQuery('#rantInput').css('fontSize');
-  
-  $('#rantInput').animate({ 
-    opacity: 0.00,
-    fontSize: "1px"
-    }, 1200, function(){
-      
-      // Animation complete - show message
-      $('#rantMessage').animate({opacity: 1.00},600);
-      
-      // Show input field again
-      $('#rantInput').val('');
-      $('#rantInput').animate({opacity: 1.00, fontSize: originalSize },600);
-      
-      updateRantCount();
-      displayTotalRantCount();
-    });
+    $('#rantInput').animate({ 
+        opacity: 0.00,
+        fontSize: '1px'
+        }, 1200, function(){
+        
+        // Animation complete - show message
+        $('#rantMessage').animate({opacity: 1.00},600);
+        
+        // Show input field again
+        $('#rantInput').val('');
+        $('#rantInput').animate({opacity: 1.00, fontSize: originalSize },600);
+        
+        incrementRantCount();
+        displayTotalRantCount();
+        });
   }
-};
+}
  
  // Clears notification message and rant message.  
  function clearMessages() {
-   $("#rantMessage").css({ opacity: 0.0 });
-   $('#notification-message').hide();
- }
- 
- // Stores an updated count of the total number of rants this user has "screamed" into roaming settings.
- // See https://msdn.microsoft.com/EN-US/library/office/fp123509.aspx#PersistRoamingSettingsMailApp
- function updateRantCount() {
-   var rantCount = getRantCount();
-   rantCount++;
-   setRantCount(rantCount);
+    $('#rantMessage').css({ opacity: 0.0 });
+    $('#notification-message').hide();
  }
  
  function getRantCount() {
-   var settings = Office.context.roamingSettings;
-   var count = settings.get("rantCount");
+    var settings = Office.context.roamingSettings;
+    var count = settings.get('rantCount');
    
-   // If rantCount has never been set, it will be null.
-   if (isNaN(parseInt(count))) {
-     count = 0;
-   }
-   return count;
+    // If rantCount has never been set, it will be null.
+    if (isNaN(parseInt(count))) {
+        count = 0;
+    }
+    return count;
  }
  
- function setRantCount(newCount) {
-   var settings = Office.context.roamingSettings;
-   settings.set("rantCount", newCount);
-   settings.saveAsync(saveSettingsCallback);
+  // Stores an updated count of the total number of rants this user has 'screamed' into roaming settings.
+ // See https://msdn.microsoft.com/EN-US/library/office/fp123509.aspx#PersistRoamingSettingsMailApp
+ function incrementRantCount() {
+  
+    var rantCount = getRantCount();
+    rantCount++;
+    var settings = Office.context.roamingSettings;
+    settings.set('rantCount', rantCount);
+    settings.saveAsync(saveSettingsCallback);
  }
  
  function displayTotalRantCount() {
-   var total = getRantCount();
+    var total = getRantCount();
    
-   $('#rantCountMessage').text("Total number of rants:" + " " + total);
+    $('#rantCountMessage').text('Total number of rants:' + ' ' + total);
  }
  
 // Saves all roaming settings.
@@ -100,7 +96,7 @@ function saveSettingsCallback(asyncResult) {
       
         // Handle the failure
         var err = asyncResult.error;
-        app.showNotification("Failed to update Rant Count", err.name + ": " + err.message);
+        app.showNotification('Failed to update Rant Count', err.name + ': ' + err.message);
     }
 } 
    
